6fb5e2d13ceeb2d175afa0eae286cc4e2ed6a7fc
maxim <max280596@gmail.com>
2021-03-05 12:13:36 +0200

    project complete

---
+++ src/Components/GoodsItem.jsx
@@ -0,0 +1,35 @@
+function GoodsItem(props) {
+    const {
+        id,
+        name,
+        description,
+        price,
+        full_background,
+        addToBasket = Function.prototype,
+    } = props;
+
+    return (
+        <div className='card'>
+            <div className='card-image'>
+                <img src={full_background} alt={name} />
+            </div>
+            <div className='card-content'>
+                <span className='card-title'>{name}</span>
+                <p>{description}</p>
+            </div>
+            <div className='card-action'>
+                <button
+                    className='btn blue-grey'
+                    onClick={() => addToBasket({ id, name, price })}
+                >
+                    Купить
+                </button>
+                <span className='right' style={{ fontSize: '1.6rem' }}>
+                    {price} руб.
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export { GoodsItem };
---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <nav className='blue-grey darken-3'>
+            <div className='nav-wrapper'>
+                <a href='!#' className='brand-logo'>
+                    React Fortnite Shop
+                </a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <a href='https://github.com/Maxim280596'>Repo</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export { Header };
